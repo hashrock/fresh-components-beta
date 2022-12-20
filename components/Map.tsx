2f14c229827ea4eef8e053b54947e83ae5371f09
hashrock <hashrock@users.noreply.github.com>
2022-12-20 21:11:35 +0900

    fit map

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,8 +5,14 @@ import world from "https://esm.sh/world-atlas@2.0.2/land-110m.json" assert {
 };
 
 export default function Map() {
+  const width = 600;
+  const height = 300;
+
   const projection = geo.geoEqualEarth();
-  
+  projection.fitSize(
+    [width, height],
+    topojson.feature(world, world.objects.land),
+  );
   const path = geo.geoPath(projection, null);
   const land = topojson.feature(world, world.objects.land);
 
@@ -14,7 +20,7 @@ export default function Map() {
     <div class="flex flex-col items-center justify-center">
       <h1 class="text-4xl font-bold">Map</h1>
       <p class="text-gray-500">This is the Map page.</p>
-      <svg viewBox="0 0 900 600" style="display: block;">
+      <svg viewBox={`0 0 ${width} ${height}`} style="display: block;">
         <g>
           <path d={path(land)}></path>
         </g>
