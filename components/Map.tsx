5a67ca78c85da11e4f15f4bae8c000e47e1fbb58
hashrock <hashrock@users.noreply.github.com>
2022-12-22 17:19:17 +0900

    use icon

--- components/Map.tsx
+++ components/Map.tsx
@@ -47,8 +47,44 @@ export default function Map(props: MapProps) {
     <div>
       {line && (
         <svg viewBox={`0 0 ${width} ${height}`} style="display: block;">
+          <defs>
+            <symbol
+              transform-origin="center"
+              id="deno-logo"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+            >
+              <g clip-path="url(#a)">
+                <path
+                  fill="#000059"
+                  d="M15 0a15 15 0 1 1 0 30 15 15 0 0 1 0-30Z"
+                />
+                <path
+                  fill="#fff"
+                  d="M14.7 22.3a.7.7 0 0 0-1 .5l-1 3.7a.7.7 0 0 0 1.5.4l1-3.7v-.4a.7.7 0 0 0-.5-.5Zm-7-3.8v.2l-1 3.7a.7.7 0 0 0 1.4.4l.9-3.4c-.5-.2-1-.5-1.3-.9Zm-2.4-4.2a.7.7 0 0 0-.8.5l-1 3.7a.7.7 0 0 0 1.4.4l1-3.7c0-.4-.2-.8-.6-1Zm22.4-.8a.7.7 0 0 0-1 .5l-1 3.7a.7.7 0 0 0 1.5.4l1-3.7c.1-.5-.1-.8-.5-1Zm-24.5-5c-.9 1.5-1.4 3.2-1.6 5l.4.2c.4 0 .8-.2.9-.5l1-3.8a.7.7 0 0 0-.7-.9Zm22 0a.7.7 0 0 0-1 .5l-1 3.8a.7.7 0 0 0 1.5.4l1-3.8c0-.4-.2-.7-.5-.8ZM7.5 5a.7.7 0 0 0-.9.5l-1 3.7a.7.7 0 0 0 1.4.4L8 6c.1-.4-.1-.8-.5-.9Zm12.8.7a.7.7 0 0 0-.9.5l-.7 2.5 1.3.8.8-2.9c.1-.4-.1-.8-.5-.9Zm-6.5-4.1c-.5 0-1 0-1.4.2l-1 3.4a.7.7 0 0 0 1.4.4l1-3.7v-.3ZM23 4l-.2.8a.7.7 0 0 0 1.4.4v-.1l-1.2-1Zm-5.8-2.4L16.8 3a.7.7 0 0 0 1.4.4l.4-1.4-1.3-.3h-.1ZM9.7 24.6a.7.7 0 0 1 1.4.4l-.7 2.7h-.1L9 27l.7-2.5Z"
+                />
+                <path
+                  fill="#fff"
+                  d="M14.4 8.5c-4.3 0-7.7 2.7-7.7 6.2 0 3.2 3 5.2 8 5.1.3 0 .5.3.6.7l.8 3.8.7 4c3.1-.4 6-2 8.2-4.3l-2.3-8.5c-.5-2-1.2-4-3-5.3a8.4 8.4 0 0 0-5.3-1.7Z"
+                />
+                <path
+                  fill="#000059"
+                  d="M15.4 11a1 1 0 1 1 0 1.8 1 1 0 0 1 0-1.8Z"
+                />
+
+              </g>
+              <defs>
+                <clipPath id="a">
+                  <path fill="#fff" d="M0 0h30v30H0z" />
+                </clipPath>
+              </defs>
+            </symbol>
+          </defs>
           <g>
-            <path fill="#aaa" d={line}></path>
+            <path fill="#E5E5E5" d={line}></path>
           </g>
           {dataCentersGeo.map((dc) => {
             return (
@@ -78,15 +114,17 @@ export default function Map(props: MapProps) {
                     </circle>
                   )
                   : (
-                    <circle
-                      cx={dc.coordinates[0]}
-                      cy={dc.coordinates[1]}
-                      r="2"
-                      class="opacity-50 group-hover:opacity-100 transition-opacity"
-                      fill="rgba(50,50,200,1)"
-                    >
-                    </circle>
-                  )}
+                    <use href="#deno-logo" transform="translate(-5, -12.5)" width={20} height={20} x={dc.coordinates[0]} y={dc.coordinates[1]} />
+                    // <circle
+                    //   cx={dc.coordinates[0]}
+                    //   cy={dc.coordinates[1]}
+                    //   r="2"
+                    //   class="opacity-50 group-hover:opacity-100 transition-opacity"
+                    //   fill="rgba(50,50,200,1)"
+                    // >
+                    // </circle>
+                  )
+                }
                 <circle
                   cx={dc.coordinates[0]}
                   cy={dc.coordinates[1]}
@@ -120,7 +158,8 @@ export default function Map(props: MapProps) {
             );
           })}
         </svg>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 }
