c8e81e37233e91ed3f6b928c82701cb0d158b012
hashrock <hashrock@users.noreply.github.com>
2023-06-19 16:27:34 +0200

    add switch

---
+++ components/Switch.tsx
@@ -0,0 +1,24 @@
+
+import { useState } from "preact/hooks";
+import { Switch as HeadlessSwitch } from "https://esm.sh/@headlessui/react@1.6.6?alias=react:preact/compat,react-dom:preact/compat,@types/react:preact/compatt&external=preact/compat";
+
+export function Switch() {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <HeadlessSwitch
+      checked={enabled}
+      onChange={setEnabled}
+      className={`${
+        enabled ? "bg-blue-600" : "bg-gray-200"
+      } relative inline-flex h-6 w-11 items-center rounded-full`}
+    >
+      <span className="sr-only">Enable notifications</span>
+      <span
+        className={`${
+          enabled ? "translate-x-6" : "translate-x-1"
+        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+      />
+    </HeadlessSwitch>
+  );
+}
--- islands/Gallery.tsx
+++ islands/Gallery.tsx
@@ -1,9 +1,9 @@
-import { useState } from "preact/hooks";
 import CodeBlock from "../components/CodeBlock.tsx";
 import Map from "../components/Map.tsx";
 
 import { Head } from "$fresh/runtime.ts";
 import IconSourceCode from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/source-code.tsx";
+import { Switch } from "../components/Switch.tsx";
 const codeExample = `import { useState } from "preact/hooks
 console.log("Hello World!");
 `;
@@ -43,6 +43,12 @@ export default function Gallery(props: GalleryProps) {
             Upstream
           </a>
         </p>
+
+        <h2 class="text-2xl font-bold">Headless UI Switch</h2>
+
+        <Switch />
+
+
 
         <h2 class="text-2xl font-bold">Map</h2>
 
