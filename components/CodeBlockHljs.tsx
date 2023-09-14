f98604ca8c7869d5da2d776ba01b28fd2284677f
hashrock <hashrock@users.noreply.github.com>
2023-09-14 14:40:25 +0900

    use core version

--- components/CodeBlockHljs.tsx
+++ components/CodeBlockHljs.tsx
@@ -1,26 +1,25 @@
-import { useState } from "preact/hooks";
-import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/highlight.min.js";
+import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/core.min.js";
+import ts from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/typescript.min.js";
 
 interface CodeBlockProps {
   code: string;
 }
 
 export default function CodeBlock(props: CodeBlockProps) {
+  hljs.registerLanguage("ts", ts);
   const result = hljs.highlight(props.code, {
     language: "ts",
   });
 
   return (
-    <div class="">
-      <div class="relative">
-        <pre class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap">
+    <div class="relative">
+      <pre class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap">
           <code
             dangerouslySetInnerHTML={{ __html: result.value }}
           >
           </code>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css"></link>
-        </pre>
-      </div>
+      </pre>
     </div>
   );
 }
