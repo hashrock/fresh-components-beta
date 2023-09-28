0805ace9c85f468c52d17e7aacf04bdbe54394cf
hashrock <hashrock@users.noreply.github.com>
2023-09-29 00:46:03 +0900

    use opacity

--- components/Float.tsx
+++ components/Float.tsx
@@ -4,29 +4,31 @@ import { useEffect, useRef, useState } from "preact/hooks";
 // import {computePosition} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.5.3/+esm';
 
 export function Float(props: JSX.HTMLAttributes<HTMLDivElement>) {
-  // const button = document.querySelector("#button");
-  // const tooltip = document.querySelector("#tooltip");
+  const [open, setOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
 
-  // useEffect(await () => {
-  //   const result = async computePosition(button, tooltip)
-
-  // }
-  const buttonRef = useRef(null);
-  const tooltipRef = useRef(null);
-
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [position, setPosition] = useState({
+    top: 0,
+    left: 0,
+    width: "max-content",
+  });
 
   useEffect(() => {
     const button = buttonRef.current;
     const tooltip = tooltipRef.current;
     (async () => {
       if (button && tooltip) {
-        const result = await computePosition(button, tooltip);
-        console.log(result);
+        console.log("button", button?.getBoundingClientRect());
+        console.log("tooltip", tooltip?.getBoundingClientRect());
+        const result = await computePosition(button, tooltip, {
+          placement: "top",
+        });
         setPosition(() => {
           return {
-            top: result.x,
-            left: result.y,
+            top: result.y,
+            left: result.x,
+            width: "max-content",
           };
         });
       }
@@ -34,18 +36,26 @@ export function Float(props: JSX.HTMLAttributes<HTMLDivElement>) {
   }, [buttonRef, tooltipRef]);
 
   return (
-    <div {...props} class="relative">
+    <div {...props}>
       <button
         id="button"
         ref={buttonRef}
         class="bg-blue-500 text-white px-4 py-2 rounded"
+        onMouseEnter={() => {
+          setOpen(true);
+        }}
+        onMouseLeave={() => {
+          setOpen(false);
+        }}
       >
         My reference element
       </button>
+
       <div
         id="tooltip"
         ref={tooltipRef}
-        class="absolute bg-gray-800 text-gray-50 rounded p-4"
+        class={"absolute bg-gray-800 text-gray-50 rounded p-4 " +
+          (open ? "" : "opacity-0")}
         style={position}
       >
         My floating element
